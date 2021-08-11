741cf8ef231efd0857c4fbd14da9e157ce7230e5
Sören <soerendip@users.noreply.github.com>
2021-08-11 16:45:16 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,6 +8,6 @@ echo "source ${path}/bashrc.sh" >> ~/.bashrc
 source ~/.bashrc
 
 echo set bell-style none >> /home/$USER/.inputrc
-echo "\e[5~": history-search-backward >> /home/$USER/.inputrc
-echo "\e[6~": history-search-forward  >> /home/$USER/.inputrc
+echo '"\e[5~": history-search-backward' >> /home/$USER/.inputrc
+echo '"\e[6~": history-search-forward'  >> /home/$USER/.inputrc
 
