60433f7d31d7dbef29c155771243491ead95374a
Soren Wacker <s.wacker@tudelft.nl>
2024-01-05 13:25:42 +0100

    ..

--- inputrc.sh
+++ inputrc.sh
@@ -1,6 +1,5 @@
-set bell-style none >> ~/.inputrc
-"\e[5~": history-search-backward >> ~/.inputrc
-"\e[6~": history-search-forward  >> ~/.inputrc
-"\e[1;5D": beginning-of-line
-"\e[1;5C": end-of-line
-
+set bell-style none
+"\e[5~": history-search-backward
+"\e[6~": history-search-forward
+"\e[1;2D": beginning-of-line
+"\e[1;2C": end-of-line
